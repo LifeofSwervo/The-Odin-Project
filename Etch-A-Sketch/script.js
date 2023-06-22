e31b3199a77e084298bf58d62e450f6bb0226313
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-06-21 21:56:14 -0500

    Board Prompt - Message

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -12,6 +12,21 @@ function createBoard(size) {
 
     for (let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
+    }
+
+}
+
+function getSize() {
+    let input = prompt("What will be the size of the board?");
+    let message = document.querySelector("#message");
+    if (input == "") { // If string input
+        message.innerHTML = "Please provide a number."
+    } else if (input <= 0 || input > 100) { // If less than 0 or more than 100
+        message.innerHTML = "Input a number between 1 and 100."
+    } else {
+        message.innerHTML = "Now you can draw!"
     }
 }
 
